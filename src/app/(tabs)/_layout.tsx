3f768fe4f81a5e10caeeecde12d4db186753210a
d3r3alk3ll0 <derealkello011@gmail.com>
2026-09-14 03:11:26 +0000

    feat: add @expo/vector-icons dependency and update TabsLayout styling

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 
 import { useTheme } from "@/Theme/useTheme";
 import { Tabs } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabsLayout() { 
     const { colors } = useTheme();
@@ -8,30 +9,44 @@ export default function TabsLayout() {
         <Tabs
             screenOptions={{
                 headerShown: false,
-                tabBarActiveTintColor: colors.primary,
+                tabBarActiveTintColor: 'blue',
                 tabBarInactiveTintColor: colors.textSecondary,
                 tabBarStyle: {
-                    backgroundColor: colors.header,
+                    // backgroundColor: '#fafbba',
                     borderTopColor: colors.border,
-                }
+                    borderRadius: 20,
+                    paddingBottom: 'auto',
+                    left: 20,
+                    right: 20,
+                    height: 65,
+                    position: 'absolute',
+                    
+                },
+                animation: "shift",
             }}
         >
             <Tabs.Screen 
                 name='Flatlist'
                 options={{
                     title: 'Home',
+                    headerBackButtonDisplayMode: "default",
+                    tabBarIcon: ( { color, size } ) => (
+                        <Ionicons name='home' size={size} color={color} />
+                    )
                 }}
             />
             <Tabs.Screen 
                 name='Buttons'
                 options={{
                     title: 'Buttons',
+                    headerBackButtonDisplayMode: "default",
                 }}
             />
             <Tabs.Screen 
                 name='Resources'
                 options={{
                     title: 'Resources',
+                    headerBackButtonDisplayMode: "default",
                 }}
             />
 
